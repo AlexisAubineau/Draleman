dd4095dc25055106e5dbe10a3913a61f46f63b74
Alexis <alexis.aubineau@ynov.com>
2020-05-04 19:29:36 +0200

    Make buttons worked but new game not work because push states not define

--- Draleman/MainMenuState.cpp
+++ Draleman/MainMenuState.cpp
@@ -22,15 +22,22 @@ void MainMenuState::initKeybinds()
 	ifs.close();
 }
 
+void MainMenuState::initButtons()
+{
+	this->buttons["GAME_STATE"] = new Button(100, 100, 150, 50,
+		&this->font, "New Game",
+		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+	this->buttons["EXIT_STATE"] = new Button(100, 300, 150, 50,
+		&this->font, "Quit",
+		sf::Color(100, 100, 100, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+}
+
 MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys)
 	: State(window, supportedKeys)
 {
 	this->initFonts();
 	this->initKeybinds();
-
-	this->gamestate_btn = new Button(100, 100, 150, 50, 
-		&this->font, "New Game", 
-		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200));
+	this->initButtons();
 
 	this->background.setSize(sf::Vector2f(window->getSize().x, window->getSize().y));
 	this->background.setFillColor(sf::Color::Magenta);
@@ -38,7 +45,10 @@ MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int
 
 MainMenuState::~MainMenuState()
 {
-	delete this->gamestate_btn;
+	auto it = this->buttons.begin();
+	for (it = this->buttons.begin(); it != this->buttons.end(); it++) {
+		delete it->second;
+	}
 }
 
 void MainMenuState::endState()
@@ -51,12 +61,39 @@ void MainMenuState::updateInput(const float& dt)
 	this->checkForQuit();
 }
 
+void MainMenuState::updateButtons()
+{
+	/*Update all the buttons in the state and handles their functionality*/
+	for (auto &it : this->buttons)
+	{
+		it.second->update(this->mousePosView);
+	}
+
+	//New Game
+	if (this->buttons["GAME_STATE"]->isPressed()) {
+		this->states.push(new MainMenuState(this->window, &this->supportedKeys));
+	}
+
+	//Quit the game 
+	if (this->buttons["EXIT_STATE"]->isPressed()) {
+		this->quit = true;
+	}
+}
+
 void MainMenuState::update(const float& dt)
 {
 	this->updateMousePosition();
 	this->updateInput(dt);
 
-	this->gamestate_btn->update(this->mousePosView);
+	this->updateButtons();
+}
+
+void MainMenuState::renderButtons(sf::RenderTarget* target)
+{
+	for (auto& it : this->buttons)
+	{
+		it.second->render(target);
+	}
 }
 
 void MainMenuState::render(sf::RenderTarget* target)
@@ -66,5 +103,6 @@ void MainMenuState::render(sf::RenderTarget* target)
 
 	target->draw(this->background);
 	
-	this->gamestate_btn->render(target);
+	this->renderButtons(target);
 }
+
