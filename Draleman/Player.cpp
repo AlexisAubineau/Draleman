733aae4d6e7ac9a55554f3a48b627967e0ab78c5
Alexis <alexis.aubineau@ynov.com>
2020-05-05 11:47:04 +0200

    Player class

---
+++ Draleman/Player.cpp
@@ -0,0 +1,26 @@
+#include "Player.h"
+
+//Initializer functions
+void Player::initVariables()
+{
+}
+
+void Player::initComponents()
+{
+	
+}
+
+//Contructors / Destructors
+Player::Player(float x, float y, sf::Texture* texture)
+{
+	this->initVariables();
+	this->initComponents();
+
+	this->createSprite(texture);
+	this->setPosition(x, y);
+}
+
+Player::~Player()
+{
+
+}
