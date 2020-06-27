2271a1b5f007d360effb3bab8e57fe0b46681c46
Alexis <alexis.aubineau@ynov.com>
2020-06-27 16:52:11 +0200

    Continue animationComponent

--- Draleman/AnimationComponent.cpp
+++ Draleman/AnimationComponent.cpp
@@ -8,27 +8,27 @@ AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_
 
 AnimationComponent::~AnimationComponent()
 {
-	
+	for (auto &i : this->animations)
+	{
+		delete i.second;
+	}
 }
 
 // Functions
 
-void AnimationComponent::addAnimation(const std::string key)
+void AnimationComponent::addAnimation(
+	const std::string key,
+	float animationTimer, 
+	int start_frame_x, int start_frame_y, int frames_x, int frames_y, int width, int height)
 {
+	this->animations[key] = new Animation(
+		this->sprite, this->textureSheet, 
+		animationTimer, 
+		start_frame_x, start_frame_y, frames_x, frames_y, width, height
+	);
 }
 
-void AnimationComponent::startAnimation(const std::string animation)
+void AnimationComponent::play(const std::string key, const float& dt)
 {
-}
-
-void AnimationComponent::pauseAnimation(const std::string animation)
-{
-}
-
-void AnimationComponent::resetAnimation(const std::string animation)
-{
-}
-
-void AnimationComponent::update(const float& dt)
-{
+	this->animations[key]->play(dt);
 }
--- Draleman/AnimationComponent.h
+++ Draleman/AnimationComponent.h
@@ -28,19 +28,19 @@ private:
 		sf::IntRect currentRect;
 		sf::IntRect endRect;
 		
-		Animation(sf::Sprite& sprite, sf::Texture& texture_sheet, float animationTimer, int start_x, int start_y, int end_x, int end_y, int width, int height)
+		Animation(sf::Sprite& sprite, sf::Texture& texture_sheet, float animationTimer, int start_frame_x, int start_frame_y, int frames_x, int frames_y, int width, int height)
 			: sprite(sprite), textureSheet(texture_sheet), animationTimer(animationTimer), width(width), height(height)
 		{
-			this->startRect = sf::IntRect(start_x, end_x, width, height);
+			this->startRect = sf::IntRect(start_frame_x * width, start_frame_y * height, width, height);
 			this->currentRect = this->startRect;
-			this->endRect = sf::IntRect(end_x, end_y, width, height);
+			this->endRect = sf::IntRect(frames_x * width, frames_y * height, width, height);
 
 			this->sprite.setTexture(this->textureSheet, true);
 			this->sprite.setTextureRect(this->startRect);
 		};
 
 		//Functions
-		void update(const float& dt)
+		void play(const float& dt)
 		{
 			//Update Timer
 			this->timer = 10.f * dt;
@@ -58,28 +58,29 @@ private:
 				{
 					this->currentRect.left = this->startRect.left;
 				}
+
+				this->sprite.setTextureRect(this->currentRect);
 			}
 		};
 		
-		void pause();
-		void reset();
+		void reset()
+		{
+			this->timer = 0.f;
+			this->currentRect = this->startRect;
+		};
 	};
 
 	sf::Sprite& sprite;
 	sf::Texture& textureSheet;
-	std::map<std::string, Animation> animations;
+	std::map<std::string, Animation*> animations;
 	
 public:
 	AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet);
 	virtual ~AnimationComponent();
 
 	//Functions
-	void addAnimation(const std::string key);
+	void addAnimation(const std::string key, float animationTimer, int start_frame_x, int start_frame_y, int frames_x, int frames_y, int width, int height);
 	
-	void startAnimation(const std::string animation);
-	void pauseAnimation(const std::string animation);
-	void resetAnimation(const std::string animation);
-	
-	void update(const float& dt);
+	void play(const std::string key, const float& dt);
 };
 #endif
